2e338260f59d394571447f2987b39e1186813a53
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-20 01:12:31 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,15 @@
 class Meal
+  attr_reader :waiter, :customer, :total, :tip
 
+  @@all = []
+
+  def initialize(waiter,customer,total,tip)
+    @name = name
+    @years = years
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
